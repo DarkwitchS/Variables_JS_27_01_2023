6ed6e335bbd058e6615de8d9750a44a7b549e183
ShirleyDanielaPerezMancilla <shirleydp1109@gmail.com>
2023-01-27 21:33:27 -0500

    Variables_JS_27_01_2023

---
+++ main.js
@@ -0,0 +1,17 @@
+
+/*String*/
+/*Number*/
+/*Boolean*/
+/*Object*/
+let nombre = "Shirley";
+let edad = 22;
+let es_mujer = true;
+let mascota ={
+    nombre: "Shaco",
+    edad: "7 años",
+    raza: "criollo",
+}
+console.log(`Mi nombre es ${nombre}`);
+console.log(`Tengo ${edad} años`);
+console.log(`¿Soy mujer?: ${es_mujer}`);
+console.log(`Mi perrito es ${mascota.nombre}, tiene ${mascota.edad}`);
